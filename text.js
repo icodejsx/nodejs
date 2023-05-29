c93bd3171039adc01a825e237d86f57c4e5498e9
Nweke kenechukwu christopher <72747787+ken286@users.noreply.github.com>
2023-05-30 00:07:16 +0100

    day 1 of 100days

--- text.js
+++ text.js
@@ -1,6 +1,16 @@
 let http = require('http');
 
 let ourApp = http.createServer(function (req, res) {
+    if (req.url == '/') {
     res.end("Hello, and welcome to our Website.")
+    }
+
+    if (req.url == '/about') {
+    res.end(" there, and welcome to our about page.")
+        
+    } 
+    res.end("we cannot find the page ya looking for")
+    
+   
 })
 ourApp.listen(5000)
